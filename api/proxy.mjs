330844b1ebad35cf067dcf883e5b2d271b14cb4d
Adam Bradley <adamdbradley@users.noreply.github.com>
2021-10-20 11:00:06 -0500

    🚤

--- api/proxy.mjs
+++ api/proxy.mjs
@@ -6,7 +6,16 @@ export default function (request, response) {
       response.status(400).send(`Missing "p" querystring`);
     } else {
       const proxyUrl = request.query.p;
-      fetch(proxyUrl).then((proxyRsp) => {
+      const proxyHeaders = {
+        'accept': request.headers.accept,
+        'accept-encoding': request.headers['accept-encoding'],
+        'cache-control': request.headers['cache-control'],
+        'if-modified-since': request.headers['if-modified-since'],
+        'if-none-match': request.headers['if-none-match'],
+        'user-agent': request.headers['user-agent'],
+      };
+
+      fetch(proxyUrl, { headers }).then((proxyRsp) => {
         const headers = [
           'age',
           'cache-control',
@@ -21,6 +30,12 @@ export default function (request, response) {
             response.setHeader(headerName, proxyRsp.headers.get(headerName));
           }
         });
+        if (proxyRsp.headers.has('server')) {
+          response.setHeader('server', `proxied (${proxyRsp.headers.get('server')})`);
+        } else {
+          response.setHeader('server', `proxied`);
+        }
+        response.setHeader('Access-Control-Allow-Origin', '*');
         response.writeHead(proxyRsp.status);
 
         proxyRsp.text().then((proxyText) => {
