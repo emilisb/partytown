c964efcb0015e7c7cb6d320b8922e14ceffddeca
Adam Bradley <adamdbradley@users.noreply.github.com>
2021-10-20 10:49:42 -0500

    🌐

---
+++ api/proxy.mjs
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch';
+
+export default function (request, response) {
+  try {
+    if (!request.params.p) {
+      response.status(400).send(`Missing "p" querystring`);
+    } else {
+      const proxyUrl = request.params.p;
+      fetch(proxyUrl).then((proxyRsp) => {
+        const headers = [
+          'age',
+          'cache-control',
+          'content-type',
+          'date',
+          'etag',
+          'last-modified',
+          'x-cache',
+        ];
+        headers.forEach((headerName) => {
+          if (proxyRsp.headers.has(headerName)) {
+            response.setHeader(headerName, proxyRsp.headers.get(headerName));
+          }
+        });
+        response.writeHead(proxyRsp.status);
+
+        proxyRsp.text().then((proxyText) => {
+          response.end(proxyText);
+        });
+      });
+    }
+  } catch (e) {
+    response.status(500).send(String(e));
+  }
+}
